623c1c2e34fc898546a03dba1e2a3f58e7c0c895
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-26 00:40:28 +0300

    This will add the first part of the random quotes carousel body

--- config.py
+++ config.py
@@ -4,7 +4,18 @@ class Config:
     '''
     General configuration parent class
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:muruthi1995@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD =os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Le Blunt'
+    SENDER_EMAIL = 'adinomuruthi1@gmail.com'
 
 class ProdConfig(Config):
     '''
@@ -21,7 +32,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:muruthi1995@localhost/pitch'
     DEBUG = True
 
 
