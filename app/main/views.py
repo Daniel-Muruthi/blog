d69f68a308e036650b77b77a077bf029a85308e5
Daniel-Muruthi <daniel.muruthi@student.moringaschool.com>
2021-09-26 16:25:20 +0300

    This will add the userblogpage

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..requests import get_quotes
+from ..requests import get_quotes, get_quotes1, get_quotes2, get_quotes3, get_quotes4
 
 @main.route('/')
 def index():
@@ -9,5 +9,9 @@ def index():
     '''
     title="Le Blunt"
     randomquotes = get_quotes()
+    randomquotes1 = get_quotes1()
+    randomquotes2 = get_quotes2()
+    randomquotes3 = get_quotes3()
+    randomquotes4 = get_quotes4()
 
-    return render_template('index.html', title=title, randomquotes = randomquotes)
+    return render_template('index.html', title=title, randomquotes = randomquotes, randomquotes1=randomquotes1, randomquotes2=randomquotes2, randomquotes3=randomquotes3, randomquotes4=randomquotes4)
